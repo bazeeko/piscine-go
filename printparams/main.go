1198ce4e33595794e231ff8062d07511795e7591
bazeeko <bazarbayev.rus@gmail.com>
2019-10-23 13:07:43 +0600

    quest 6-2

---
+++ printparams/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "os"
+import "github.com/01-edu/z01"
+
+func main() {
+	length := 0
+	for range os.Args {
+		length++
+	}
+	for index := range os.Args {
+		if index == length-1 {
+			break
+		}
+		for _, char := range os.Args[index+1] {
+			z01.PrintRune(char)
+		}
+		z01.PrintRune(10)
+	}
+}
