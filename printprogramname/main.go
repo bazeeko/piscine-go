aea25cadf34f8ea4a5944b44ad850f06b96f5e28
bazeeko <bazarbayev.rus@gmail.com>
2019-10-23 12:34:27 +0600

    print program name

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -5,7 +5,7 @@ import "github.com/01-edu/z01"
 
 func main() {
 	programname := os.Args[0]
-	for _, char := range programname{
+	for _, char := range programname {
 		z01.PrintRune(char)
 	}
 	z01.PrintRune(10)
